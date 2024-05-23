6e9666384153c7819e3cb7d82f5dd8e09912027b
MrNikzOp <166023830+MrNikzOp@users.noreply.github.com>
2024-05-23 21:10:17 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -2,9 +2,9 @@ from os import environ
 class Config(object):
     API_ID = environ.get("API_ID", "20204401")
     API_HASH = environ.get("API_HASH", "6ec374e7e7492de06a9b22bb746bd20f")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "6922036916:AAHod6WEiUf70eq6ytG2P1uRrk6__uQ-xHU")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7037251367:AAH4B0G5Xj-kxdzwJbHt7IRWCUXYCQw_frs")
     STRING_SESSION = environ.get("STRING_SESSION", "")
-    SUDO_USERS = environ.get("SUDO_USERS", "6168162777 6366990600")
+    SUDO_USERS = environ.get("SUDO_USERS", "6807518752 6366990600")
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     💢 **ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ ᴛʜᴇ ʙᴏᴛ ᴀʀᴇ:**
